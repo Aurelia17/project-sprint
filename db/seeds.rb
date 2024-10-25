f6e160f3251e43812ec66e4b19f741fc22d25158
aurelia ritter <aureliaritter@gmail.com>
2024-10-25 20:14:39 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,51 @@
-
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-#
 require "open-uri"
-require 'faker'
 
 puts "cleaning DB..."
-Flat.destroy_all
+User.destroy_all # Destroy all existing users from database to avoid duplicates
+Flat.destroy_all # Destroy all existing flats from database to avoid duplicates
 
-puts "Creating flats..."
-
-user1 = User.new(first_name: 'John',
-                 last_name: 'Doe',
-                 email: 'test@test.com',
-                 password: '123456',
-                 password_confirmation: '123456',)
-user1.save!
+# Creating a new user
+User.create!(
+            email: 'test@test.com',
+            password: '123456',
+            password_confirmation: '123456')
 puts "User 1 created !"
 
-flats_names = ["Charming Villa", "Astonishing Manoir", "Cozy Cottage", "Elegant Estate", "Luxurious Retreat", "Modern Loft", "Spacious Bungalow", "Rustic Cabin", "Stylish Penthouse", "Sunny Beach House", "Serene Hideaway", "Enchanting Farmhouse", "Grand Chateau", "Beautiful Townhouse", "Contemporary Residence", "Chic Designer Home", "Secluded Oasis", "Welcoming Family Home", "Quaint Country House", "Exquisite Mansion", "Peaceful Getaway", "Coastal Dream House", "Bohemian Villa", "Timeless Colonial House", "Artistic Studio", "Idyllic Cottage", "Hidden Gem House", "Classic Victorian Home", "Tranquil Zen House", "Unique Dome House"]
-flats_names.shuffle.each do |name|
-  image = URI.open("https://source.unsplash.com/random/?house")
-  flat = Flat.new(
-    name: flats_name,
-    price: rand(60..120),
-    address: Faker::Address.full_address
-  )
-  flat.photo.attach(io: image, filename: 'gadjak', content_type: 'image/png')
-  flat.save!
-  puts "#{flat.name} has been added!"
+puts "Creating flats..."
+url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+flats = JSON.parse(URI(url).read)
+
+# 5.times do
+#   flats.each do |flat|
+#     flat = Flat.new(
+#       name: flat['name'],
+#       price: flat['price'],
+#       address: "Paris, France"
+#     )
+#   image = URI.open("#{flat['imageUrl']}")
+#   filename = (100..9999999).random.to_s
+#   flat.photo.attach(io: image, filename: filename, content_type: 'image/png')
+#   flat.save!
+#   end
+# end
+
+
+flats.each do |flat|
+  begin
+    flat_record = Flat.new(
+      name: flat['name'],
+      price: flat['price'],
+      address: "Paris, France"
+    )
+
+    image_url = flat['imageUrl']
+    puts "Image URL: #{image_url}" # Log the URL to check
+    image = URI.open(image_url)
+    filename = File.basename(image_url) # Get the filename from the URL
+
+    flat_record.photo.attach(io: image, filename: filename, content_type: 'image/png')
+    flat_record.save!
+  rescue => e
+    puts "Error processing flat #{flat['name']}: #{e.message}"
+  end
 end
